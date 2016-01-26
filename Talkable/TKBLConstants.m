61275d80ecca2049caf59d48d0e13901c21010e6
vitalikdanchenko <devyjones@gmail.com>
2016-01-26 18:42:54 +0200

    bump version to 1.3.1

--- Talkable/TKBLConstants.m
+++ Talkable/TKBLConstants.m
@@ -8,7 +8,7 @@
 
 #import "TKBLConstants.h"
 
-NSString* TKBLVersion                                   = @"1.3.0";
+NSString* TKBLVersion                                   = @"1.3.1";
 
 NSString* TKBLErrorDomain                               = @"com.talkable.ios-sdk";
 
