1be4004022144a040d00eb6e45298791d4490fff
Michael Grischenko <robotcrimefighter@gmail.com>
2018-03-16 13:14:20 +0200

    bump version to 1.4.5 (#17)

--- Talkable/TKBLConstants.m
+++ Talkable/TKBLConstants.m
@@ -8,7 +8,7 @@
 
 #import "TKBLConstants.h"
 
-NSString* TKBLVersion                                   = @"1.4.4";
+NSString* TKBLVersion                                   = @"1.4.5";
 
 NSString* TKBLErrorDomain                               = @"com.talkable.ios-sdk";
 
