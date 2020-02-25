b3c50ec93c4e3c424daa0f4bdc63465567915cab
kostilal <i.kostyukevich@gmail.com>
2020-02-25 18:09:45 +0200

    bump version 1.4.11

--- Talkable/TKBLConstants.m
+++ Talkable/TKBLConstants.m
@@ -8,7 +8,7 @@
 
 #import "TKBLConstants.h"
 
-NSString* TKBLVersion                                   = @"1.4.10";
+NSString* TKBLVersion                                   = @"1.4.11";
 
 NSString* TKBLErrorDomain                               = @"com.talkable.ios-sdk";
 
