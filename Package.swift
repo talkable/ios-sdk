ac6553c7005d1d124749d9c1317aa37e2e935cd7
Taras Dehtiar <dgtv@users.noreply.github.com>
2022-07-19 15:24:42 +0200

    Swift Package Manager support (#16)

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let version = "1.4.12"
+// Checksum of the file at `sdkUrl` link. To generate: `swift package compute-checksum build/talkable_ios_sdk.zip`
+let checksum = "a71ababfedb1fa485258a4928ed00891f08cff2c06b77ccddd28f538e192b580"
+let sdkUrl = "https://github.com/talkable/ios-sdk/releases/download/\(version)/talkable_ios_sdk_\(version).zip"
+
+let package = Package(
+    name: "TalkableSDK",
+    platforms: [.iOS(.v9)],
+    products: [
+        .library(
+            name: "TalkableSDK",
+            targets: ["TalkableSDK"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "TalkableSDK",
+            url: sdkUrl,
+            checksum: checksum
+        )
+    ]
+)
